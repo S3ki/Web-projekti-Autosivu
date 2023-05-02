51a3f27f7789b8ddb4f0858e92c113cc261e3e12
S3ki <92526584+S3ki@users.noreply.github.com>
2023-05-02 14:22:54 +0300

    lisa

--- database.js
+++ database.js
@@ -4,5 +4,19 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'aforum'
+  database: 'aforum',
+  password: ''
 });
+
+connection.connect(function(error){
+	if(error)
+	{
+		throw error;
+	}
+	else
+	{
+		console.log('MySQL Database is connected Successfully');
+	}
+});
+
+module.exports = connection;
